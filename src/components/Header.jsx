90bf3e1b8da2ddf82ba69b5548df85e94f63d309
Jesper Kling <jesperkling@outlook.com>
2023-08-23 17:43:38 +0200

    Header design is done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,19 @@
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { BiSolidUser } from "react-icons/bi";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Navbar bg="light" variant="light p-0">
+      <Navbar.Brand className="mx-auto">BabyFriendly</Navbar.Brand>
+      <Nav>
+        <Nav.Link href="#home" className="h-100 bg-warning text-success">
+          <BiSolidUser className="me-2 text-success" />
+          Log in
+        </Nav.Link>
+      </Nav>
+    </Navbar>
+  );
 };
 
 export default Header;
